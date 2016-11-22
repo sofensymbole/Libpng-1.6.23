e7d24f0c1547a7add291fd835153ebf49b01805f
sofensymbole <bhardwaj.prateek17@gmail.com>
2016-11-22 11:12:52 +0530

    Add files via upload

---
+++ obj/local/armeabi-v7a/objs/png/arm/filter_neon.o.d
@@ -0,0 +1,8 @@
+obj/local/armeabi-v7a/objs/png/arm/filter_neon.o: jni/arm/filter_neon.S \
+ jni/pngpriv.h jni/pnglibconf.h jni/png.h
+
+jni/pngpriv.h:
+
+jni/pnglibconf.h:
+
+jni/png.h:
---
+++ obj/local/armeabi-v7a/objs/png/pngtest.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/png/pngtest.o: jni/pngtest.c \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h \
+ jni/png.h jni/pnglibconf.h jni/pngconf.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/setjmp.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/setjmp.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/time.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/zlib.h jni/zconf.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/pathconf.h
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+jni/png.h:
+
+jni/pnglibconf.h:
+
+jni/pngconf.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/setjmp.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/setjmp.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/time.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/zlib.h:
+
+jni/zconf.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/prateek/Android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/pathconf.h:
