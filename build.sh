e7f89581d4c6e78712fe75c73d9f0ca2cd789119
sofensymbole <bhardwaj.prateek17@gmail.com>
2016-11-22 11:07:17 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/home/prateek/Android/android-ndk-r8e/ndk-build NDK_PROJECT_PATH=./ $@
---
+++ jni/Application.mk
@@ -0,0 +1,4 @@
+APP_OPTIM := release
+APP_ABI := all # or armeabi
+APP_MODULES := libpng
+NDK_TOOLCHAIN_VERSION := 4.6
